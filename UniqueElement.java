7d5149217d0bea52f3bd208e72079475bcbc3626
Abul Fajle Azad <abulfajle405@gmail.com>
2022-12-24 22:13:28 +0530

    Next Commit

--- UniqueElement.java
+++ UniqueElement.java
@@ -1,14 +1,20 @@
-
+import java.io.*;
+import java.util.*;
 public class UniqueElement{
     public static void main(String[] args){
-        int arr[] = {10, 5,8,  1 , 10, 4, 5, 9, 3, 8 };
+        // using brut force
+        int arr[] = {10, 5,8, 1 , 10, 4, 5, 9, 3, 8 };
+        System.out.println("Unique elements are");
 
-        for(int i = 0; i <arr.length; i++){
-            for(j=0; j<arr.length;j++){
-                if(i==j){
-                    
-                }
+        int n = 10;
+        for(int i =0; i <n; i ++){
+            int num = 0;
+            for(int j =0; j <n; j++){
+                if(arr[j]==arr[i])num++;
             }
+            if(num==1)System.out.print(arr[i]+" ");
         }
+        // Time complexity is O(n^2)
+       
     }
 }
